3e86b03de03f7dbc2c84875b754c223d4db193b6
rezmes <rezmes@gmail.com>
2024-06-19 12:06:18 +0330

    Changed to SPFx Project

---
+++ src/webparts/reactjs/ReactjsWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ReactjsWebPartStrings';
+import Reactjs from './components/Reactjs';
+import { IReactjsProps } from './components/IReactjsProps';
+
+export interface IReactjsWebPartProps {
+  description: string;
+}
+
+export default class ReactjsWebPart extends BaseClientSideWebPart<IReactjsWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IReactjsProps > = React.createElement(
+      Reactjs,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/reactjs/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactjsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactjsWebPartStrings' {
+  const strings: IReactjsWebPartStrings;
+  export = strings;
+}
